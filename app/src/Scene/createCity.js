c0c18085d88a95501209da423f74bb5205ec47d9
Rafael Nunes <rafaelngds@gmail.com>
2019-11-26 18:01:56 +1100

    fix(createCity): fixes project size

--- app/src/Scene/createCity.js
+++ app/src/Scene/createCity.js
@@ -1,19 +1,31 @@
 import get from 'lodash/get';
 
 import blocksPallet from '../blocksPallet';
+const ROAD_SIZE_OFFSET = 1;
 
 function getSize(block) {
-  const roadSize = 1;
   const childCount = block.children.length;
-  const roadsOffset = childCount * roadSize;
+  const roadsOffset = childCount * ROAD_SIZE_OFFSET;
   const x = block.loc + roadsOffset;
-  const y = block.type === 'FunctionDeclaration' ? block.cec : 1;
+  const y = block.type === 'FunctionDeclaration' ? block.cec || 1 : 1;
   const z = block.loc + roadsOffset;
   return [x, y, z];
 }
 
+function getProjectSize(cityBlocks) {
+  return cityBlocks['Project'].children.reduce(
+    (acc, childId) => {
+      const size = get(cityBlocks, `${childId}.size`);
+      const x = size[0] + acc[0];
+      const y = size[1];
+      const z = size[2] + acc[2];
+      return [x, y, z];
+    },
+    [1, 1, 1]
+  );
+}
+
 function buildCityBlocks(blocksMap) {
-  const roadSize = 1;
   const cityBlocks = {};
   const root = blocksMap['Project'];
   const queue = [root.id];
@@ -24,8 +36,7 @@ function buildCityBlocks(blocksMap) {
   cityBlocks[root.id] = {
     ...root,
     color: blocksPallet[root.type],
-    position: [childOffsetX, childOffsetY, childOffsetZ],
-    size: getSize(root)
+    position: [childOffsetX, childOffsetY, childOffsetZ]
   };
 
   while (queue.length) {
@@ -37,13 +48,21 @@ function buildCityBlocks(blocksMap) {
     const parentZ = get(cityBlocks[block.parent], 'position[2]', 0);
     childOffsetX = parentX;
     childOffsetZ = parentZ;
-    if (get(cityBlocks, `${block.id}.type`) !== 'FunctionDeclaration') {
-      childOffsetY = block.position[1];
-    } else {
-      // size/2 because rendering on Y axis renders half on up direction
-      // and another half on down direction.
-      // plus 0.5 because
+    const type = get(cityBlocks, `${block.id}.type`);
+    if (type === 'FunctionDeclaration') {
+      /*
+       * block.position[1]: means the current block Y position.
+       * size/2: because rendering on Y axis renders half on up direction and another half on down direction.
+       */
       childOffsetY = block.position[1] + block.size[1] / 2;
+    } else if (type === 'Project') {
+      /*
+       * block.position[1]: means the current block Y position.
+       * 0.5: because Project have 1 of height.
+       */
+      childOffsetY = block.position[1] + 0.5;
+    } else {
+      childOffsetY = block.position[1];
     }
 
     for (let i = 0; i < block.children.length; i++) {
@@ -64,13 +83,15 @@ function buildCityBlocks(blocksMap) {
       };
 
       // Update offset based on its size
-      childOffsetX = child.loc + roadSize;
-      childOffsetZ = child.loc + roadSize;
+      childOffsetX = child.loc + ROAD_SIZE_OFFSET;
+      childOffsetZ = child.loc + ROAD_SIZE_OFFSET;
 
       // Add child to the processing queue
       queue.unshift(child.id);
     }
   }
+
+  cityBlocks[root.id].size = getProjectSize(cityBlocks);
 
   return cityBlocks;
 }
