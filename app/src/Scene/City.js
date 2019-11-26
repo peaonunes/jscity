0cf94c4833fb0858c820ace63be0a66b40b39add
Rafael Nunes <rafaelngds@gmail.com>
2019-11-26 18:01:17 +1100

    feat(City): change plane color

--- app/src/Scene/City.js
+++ app/src/Scene/City.js
@@ -6,7 +6,7 @@ import Block from './Block';
 const Plane = () => (
   <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.5, 0]} receiveShadow>
     <planeBufferGeometry attach="geometry" args={[100, 100]} />
-    <meshPhysicalMaterial attach="material" color="#4daf4a" />
+    <meshPhysicalMaterial attach="material" color="#377eb8" />
   </mesh>
 );
 
