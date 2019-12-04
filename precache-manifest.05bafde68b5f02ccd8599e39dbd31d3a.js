6999709a453a20b654b74d533b668df9ad1aaac2
Rafael Nunes <rafaelngds@gmail.com>
2019-12-04 20:41:24 +1100

    Updates

---
+++ precache-manifest.05bafde68b5f02ccd8599e39dbd31d3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb526de2325982a558954774b8f1e210",
+    "url": "/jscity/index.html"
+  },
+  {
+    "revision": "3cbb8c41e5ecf532bfb7",
+    "url": "/jscity/static/css/main.088a249b.chunk.css"
+  },
+  {
+    "revision": "b409f517b3c389820f8c",
+    "url": "/jscity/static/js/2.1a51f623.chunk.js"
+  },
+  {
+    "revision": "3cbb8c41e5ecf532bfb7",
+    "url": "/jscity/static/js/main.44913eaa.chunk.js"
+  },
+  {
+    "revision": "3742c1ef33a0588bbed0",
+    "url": "/jscity/static/js/runtime-main.0f99ab63.js"
+  }
+]);
